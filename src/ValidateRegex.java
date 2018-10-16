6cb43df684f5970e645f35ab14b35fb68c535618
Lindsay Karty <lmkarty@gmail.com>
2018-10-16 09:24:36 -0400

    added while loop

--- src/ValidateRegex.java
+++ src/ValidateRegex.java
@@ -4,25 +4,28 @@ public class ValidateRegex {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 
-		String userName = Validator.getStringMatchingRegex(scan, "Please enter your name: ", "[a-zA-Z]+");
-		scan.nextLine(); // need to clear out scanner to avoid infinite loop
 
+		do {
+			String userName = Validator.getStringMatchingRegex(scan, "Please enter your name: ", "[a-zA-Z]+");
 
-		String userPhone = Validator.getStringMatchingRegex(scan,
-				"Please enter a valid phone number using format ddd-ddd-dddd: ", "^\\d{3}-\\d{3}-\\d{4}$");
-		
-		scan.nextLine(); // need to clear out scanner to avoid infinite loop
+			String userPhone = Validator.getStringMatchingRegex(scan,
+					"Please enter a valid phone number using format ddd-ddd-dddd: ", "^\\d{3}-\\d{3}-\\d{4}$");
 
-		
-		String userEmail = Validator.getStringMatchingRegex(scan, "Please enter a valid email: ",
-				"^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$");
+			String userEmail = Validator.getStringMatchingRegex(scan, "Please enter a valid email: ",
+					"^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$");
+
+			String userDate = Validator.getStringMatchingRegex(scan,
+					"Please enter a valid date using format: dd/mm/yyyy ",
+					"^^[0-3]?[0-9]/[0-3]?[0-9]/(?:[0-9]{2})?[0-9]{2}$");
+
+			System.out.println("Thank you for your input!");
+
+			System.out.println("\nDo you want to continue? (y/n)");
+
+		} while (scan.next().equalsIgnoreCase("Y"));
+
+		scan.close();
+
 	}
-	scan.nextLine(); // need to clear out scanner to avoid infinite loop
-
-	
-	String userDate = Validator.getStringMatchingRegex(scan, "Please enter a valid date: ",
-			"^\\w+@[a-zA-Z_]+?\\.[a-zA-Z]{2,3}$");
-}
-
 
 }
